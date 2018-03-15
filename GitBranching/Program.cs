a08d1cf8d8267414010fb573a198abc571100d82
Paulow98 <paulow98@hotmail.com>
2018-03-15 11:50:21 +0000

    Fixed the message again

--- GitBranching/Program.cs
+++ GitBranching/Program.cs
@@ -10,7 +10,7 @@ namespace GitBranching
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Adeus");
         }
     }
 }
