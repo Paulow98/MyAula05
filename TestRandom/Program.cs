dd100f80f6b441973aa3157f222ebbf89bae7e22
Paulow98 <paulow98@hotmail.com>
2018-03-15 12:54:59 +0000

    Thats all

---
+++ TestRandom/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Amount of dices:");
+            int nDados = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            for(int i = 0; i< nDados; i++)
+            {
+                Console.WriteLine($"You rolled a {random.Next(1, 7)}");
+            }
+        }
+    }
+}
